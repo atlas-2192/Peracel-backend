2a9c5954acb5fce91fe4b6bddfc1ffb5a0598cbf
-v <kevinlewis0628@gmail.com>
2024-11-08 15:47:23 +0900

    feat: add JwtStrategy -create JwtAuthGuard -add getMe route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/token-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +22,11 @@ export class UsersController {
   createUser(@Body() request: CreateUserRequest) {
     console.log(request);
     return this.usersService.createUser(request);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
